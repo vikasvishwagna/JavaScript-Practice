21238a95676dfadb0074323b09127543f914ad6c
vikasvishwagna <vishwagnavikassai@gmail.com>
2025-02-04 14:26:30 +0530

    construcors notes is created

---
+++ Constructors.js/c1.js
@@ -0,0 +1,14 @@
+           //This: this call current obj.
+/*const animal ={
+  name : "Tiger",
+  age : 24,
+ nature(){
+    console.log(this);
+    console.log(this.name);
+  }
+}
+animal.area = function(){
+  console.log("lives in Bengal");
+}
+animal.nature();*/
+
