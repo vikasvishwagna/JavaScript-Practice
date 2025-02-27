8e45ca56bb5be949f3f0176717f717d8ae1dd21e
vikasvishwagna <vishwagnavikassai@gmail.com>
2025-02-27 19:03:47 +0530

    feat: added notes on callbacks

---
+++ callBacks/c1.js
@@ -0,0 +1,10 @@
+function greet(x,callback){
+  console.log("nice to see u",x);
+  callback();
+}
+
+function hello(){
+  console.log(`this is a hello function`);
+}
+
+greet("name",hello);
---
+++ callBacks/c2.js
@@ -0,0 +1,21 @@
+function orderPizza(x,callback){
+  setTimeout(()=>{
+    console.log("i need chicken tikka pizza!!");
+    reviewPizza();//it will not wait, as it takes 6sec, it excuted the next fun.
+    eatPizza();
+  },3000);
+}
+
+function eatPizza(){
+  console.log("eating pizza...");
+}
+
+function reviewPizza(){
+  setTimeout(()=>{
+    console.log("pizza tastes good!!");
+  },6000);
+}
+
+console.log("calling dominos...");
+orderPizza("name",reviewPizza);//it will not wait.
+console.log("finally done eating and reviewing the pizza");
